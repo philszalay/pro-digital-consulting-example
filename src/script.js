920011506b95181c4eb7a6a6b3cca69f269c44a1
Phil Szalay <philszalay@aol.de>
2024-05-28 12:53:20 +0200

    Allow to change texture and dimensions

--- src/script.js
+++ src/script.js
@@ -3,54 +3,65 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import Stats from 'three/examples/jsm/libs/stats.module'
 
+import woodTexture1 from '../assets/textures/wood_texture_1.jpg'
+import woodTexture2 from '../assets/textures/wood_texture_2.jpg'
+
+
 export default class ThreeJsDraft {
-  constructor () {
+  constructor() {
     /**
      * Variables
      */
-    this.canvas = document.querySelector('canvas.webgl')
-    this.width = window.innerWidth
-    this.height = window.innerHeight
-    this.devicePixelRatio = window.devicePixelRatio
+    this.canvas = document.querySelector('canvas.webgl');
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    this.devicePixelRatio = window.devicePixelRatio;
+
+    this.boxLength = 10;
+    this.boxWidth = 2;
+    this.boxStrength = 0.5;
 
     /**
      * Scene
      */
-    this.scene = new THREE.Scene()
+    this.scene = new THREE.Scene();
 
     /**
      * Camera
      */
-    this.camera = new THREE.PerspectiveCamera(75, this.width / this.height, 0.1, 1000)
-    this.camera.position.z = 5
+    this.camera = new THREE.PerspectiveCamera(75, this.width / this.height, 0.1, 1000);
+    this.camera.position.z = 15;
 
     /**
      * Renderer
      */
     this.renderer = new THREE.WebGLRenderer({
       canvas: this.canvas
-    })
-    this.renderer.setSize(this.width, this.height)
-    this.renderer.setPixelRatio(Math.min(this.devicePixelRatio, 2))
+    });
+    this.renderer.setSize(this.width, this.height);
+    this.renderer.setPixelRatio(Math.min(this.devicePixelRatio, 2));
+    this.renderer.setClearColor('white');
 
     /**
      * Controls
      */
-    this.orbitControls = new OrbitControls(this.camera, this.canvas)
+    this.orbitControls = new OrbitControls(this.camera, this.canvas);
+    this.orbitControls.enableZoom = false;
+    this.orbitControls.enablePan = false;
 
     /**
      * Resize
      */
     window.addEventListener('resize', () => {
-      this.width = window.innerWidth
-      this.height = window.innerHeight
-      this.camera.aspect = this.width / this.height
-      this.camera.updateProjectionMatrix()
+      this.width = window.innerWidth;
+      this.height = window.innerHeight;
+      this.camera.aspect = this.width / this.height;
+      this.camera.updateProjectionMatrix();
 
-      this.devicePixelRatio = window.devicePixelRatio
+      this.devicePixelRatio = window.devicePixelRatio;
 
-      this.renderer.setSize(this.width, this.height)
-      this.renderer.setPixelRatio(Math.min(this.devicePixelRatio, 2))
+      this.renderer.setSize(this.width, this.height);
+      this.renderer.setPixelRatio(Math.min(this.devicePixelRatio, 2));
     }, false)
 
     /**
@@ -59,7 +70,7 @@ export default class ThreeJsDraft {
     this.loadingManager = new THREE.LoadingManager()
 
     this.loadingManager.onStart = function (url, itemsLoaded, itemsTotal) {
-      console.log('Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.')
+      console.log('Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
     }
 
     this.loadingManager.onLoad = function () {
@@ -67,61 +78,74 @@ export default class ThreeJsDraft {
     }
 
     this.loadingManager.onProgress = function (url, itemsLoaded, itemsTotal) {
-      console.log('Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.')
+      console.log('Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
     }
 
     this.loadingManager.onError = function (url) {
-      console.log('There was an error loading ' + url)
+      console.log('There was an error loading ' + url);
     }
-
-    /**
-     * Load Assets
-     */
-    this.loadAssets()
 
     /**
      * Helpers
      */
-    this.addHelpers()
+    this.addHelpers();
 
     /**
      * Objects
      */
-    this.addObjects()
+    this.addObjects();
 
-    this.renderer.render(this.scene, this.camera)
     /**
      * Animation Loop
      */
-    this.animate()
+    this.animate();
   }
 
-  loadAssets () {
-    // const textureLoader = new THREE.TextureLoader(this.loadingManager)
+  addHelpers() {
+    const axisHelper = new THREE.AxesHelper(3);
+    this.scene.add(axisHelper);
+
+    this.stats = Stats();
+    document.body.appendChild(this.stats.dom);
   }
 
-  addHelpers () {
-    const axisHelper = new THREE.AxesHelper(3)
-    this.scene.add(axisHelper)
+  addObjects() {
+    const texture = new THREE.TextureLoader().load(woodTexture1);
 
-    this.stats = Stats()
-    document.body.appendChild(this.stats.dom)
+    const boxGeometry = new THREE.BoxGeometry(this.boxWidth, this.boxLength, this.boxStrength);
+    const boxMaterial = new THREE.MeshBasicMaterial({ map: texture });
+
+    this.box = new THREE.Mesh(boxGeometry, boxMaterial);
+
+    this.scene.add(this.box);
   }
 
-  addObjects () {
-    const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
-    const cubeMaterial = new THREE.MeshBasicMaterial({
-      color: 0x00ff00
-    })
-    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
-    this.scene.add(cube)
+  changeWoodTexture(key) {
+    switch (key) {
+      case '1':
+        new THREE.TextureLoader().load(woodTexture1, (texture) => {
+          this.box.material.map = texture;
+        })
+        break;
+      case '2':
+        new THREE.TextureLoader().load(woodTexture2, (texture) => {
+          this.box.material.map = texture;
+        });
+        break;
+      default:
+        break;
+    }
   }
 
-  animate () {
-    this.orbitControls.update()
-    this.stats.update()
-    this.renderer.render(this.scene, this.camera)
-    window.requestAnimationFrame(this.animate.bind(this))
+  updateBoxGeometry() {
+    this.box.geometry = new THREE.BoxGeometry(this.boxWidth, this.boxLength, this.boxStrength);
+  }
+
+  animate() {
+    this.orbitControls.update();
+    this.stats.update();
+    this.renderer.render(this.scene, this.camera);
+    window.requestAnimationFrame(this.animate.bind(this));
   }
 }
 
@@ -129,4 +153,25 @@ export default class ThreeJsDraft {
  * Create ThreeJsDraft
  */
 // eslint-disable-next-line no-new
-new ThreeJsDraft()
+const draft = new ThreeJsDraft();
+
+window.changeWoodTexture = function (key) {
+  draft.changeWoodTexture(key);
+}
+
+window.setWidth = function (event) {
+  draft.boxWidth = event.target.value;
+  draft.updateBoxGeometry();
+}
+
+window.setLength = function (event) {
+  draft.boxLength = event.target.value;
+  draft.updateBoxGeometry();
+}
+
+window.setStrength = function (event) {
+  draft.boxStrength = event.target.value;
+  draft.updateBoxGeometry();
+}
+
+
